231478a0df9cedfab15b0000b181c826dbdc9952
Đinh Viết Huy <154805218+dinhviethuy@users.noreply.github.com>
2024-09-26 12:28:12 +0700

    Update request.jsx

--- src/utils/request.jsx
+++ src/utils/request.jsx
@@ -1,4 +1,4 @@
-const API_DOMAIN = "http://localhost:3002/";
+const API_DOMAIN = "https://database-it-jobs.vercel.app/";
 
 export const get = async (path) => {
   const response = await fetch(`${API_DOMAIN}${path}`);
